1e7b50d385151f11309798dae994df61f9495dbb
Angell113 <114631163+Angell113@users.noreply.github.com>
2022-11-06 23:01:12 -0300

    Update shiftwords.py

--- shiftwords.py
+++ shiftwords.py
@@ -18,28 +18,3 @@ def ascii_shift_plus(key, text):
     return result
 
  
-# sleep(1000)
- 
-# while True:
-#     text = input("Enter key: ")
-#     key = int(text)
- 
-#     text = input("Enter printable character(s): ")
- 
-#     result = ascii_shift(key, text)
-    
-#     print("result:", result)
-#     print()
-#     break
-
-
-
-
-# import base64
-
-# base64_message = 'UHl0aG9uIGlzIGZ1bg=='
-# base64_bytes = base64_message.encode('ascii')
-# message_bytes = base64.b64decode(base64_bytes)
-# message = message_bytes.decode('ascii')
-
-# print(message)
